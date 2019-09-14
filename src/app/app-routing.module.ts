3ece0964fcf5f7ef56092a38806ab23fd7f6ab11
Sarah <sarahhargreaves10@gmail.com>
2019-09-14 00:04:19 -0400

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageThreeComponent } from './page-three/page-three.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{path: 'page-one', component: PageOneComponent},
+                        {path: 'page-two', component: PageTwoComponent},
+                        {path: 'page-three', component: PageThreeComponent},
+                        {path: '', redirectTo: '/page-one', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
